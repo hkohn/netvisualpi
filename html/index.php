5b4dcd898060b68c39587193c8289cfa194234d5
Holger Kohn <holger@kohn-nf.de>
2020-09-02 09:19:40 +0200

    Adding html files in html subfolder

---
+++ html/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<meta name="generator" content="Bluefish 2.2.10" >
+<meta name="author" content="Holger Kohn">
+<meta name="date" content="2020-08-24T15:36:22+0200" >
+<meta name="copyright" content="Holger Kohn">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta http-equiv="expires" content="0">
+<meta >
+</head>
+
+<body bgcolor="#404040">
+<table border="0" align="center" bgcolor="#FFFFFF" width="100%">
+<tr bgcolor="#1F2737">
+<td>
+<table border="0" width="100%">
+<tr><td width="100" align="center">
+<table><tr>
+<td>
+</td>
+<td bgcolor="#ffffff" align="center"><a href="/index.php" ><img src="netvisualpi-tux.png" width="100" height="100" alt=""><BR>NetvisualPi</a></td>
+</tr></table>
+</td>
+<td>
+<table border="1" width="100%">
+<tr>
+<!--td width="25%" align="center" bgcolor="#E6E6FA">Netzübersicht</td-->
+<td width="4%" bgcolor="#008000"></td>
+<td width="24%" align="center"><a href="/index.php?section=dashboard"><span style="color:white">Dashboard</span></a></td>
+<td width="24%" align="center"><a href="/index.php?section=netoverview"><span style="color:white">Netzübersicht</span></a></td>
+<td width="24%" align="center"><a href="/index.php?section=ipanalyse"><span style="color:white">IP-Analyse</span></a></td>
+<td width="24%" align="center"></td>
+</tr>
+</table>
+<table border="1" width="100%">
+<tr>
+<!--td width="25%" align="center" bgcolor="#E6E6FA">Netzübersicht</td-->
+<td width="4%" bgcolor="#FFA500"></td>
+<td width="24%" align="center" height="20"><!--a href="/index.php?section=antivirus"><span style="color:white">Antivirus</span></a--></td>
+<td width="24%" align="center"><!--a href="/index.php?section=urlfilter"><span style="color:white">URL-Filter</span></a--></td>
+<td width="24%" align="center"><!--a href="/index.php?section=ipsperre"><span style="color:white">IP-Sperre</span></a--></td>
+<td width="24%" align="center"></td>
+</tr>
+</table>
+<table border="1" width="100%">
+<tr>
+<!--td width="25%" align="center" bgcolor="#E6E6FA">Netzübersicht</td-->
+<td width="4%" bgcolor="#FF0000"></td>
+<td width="24%" align="center"><a href="/index.php?section=logfiles"><span style="color:white">Logdateien</span></a></td>
+<td width="24%" align="center"><a href="/index.php?section=firewall"><span style="color:white">Firewall</span></a></td>
+<td width="24%" align="center"></td>
+<td width="24%" align="center"><!--a href="/index.php?section=Setup"><span style="color:white">Setup</span--></a></td>
+</tr>
+</table>
+</td></tr>
+</table>
+</td>
+</tr>
+<tr>
+<td>
+<?php
+if ($_GET['section']=="netoverview") {
+  if ($_GET['submit']=="Tagesanzeige") {
+      $output = shell_exec('A=`ls -rt /var/www/html/files/logday*.log | tail -1`; perl /home/pi/perl/log2dot.pl $A > /var/www/html/files/tmp.dot');
+  } else {
+    $output = shell_exec('perl /home/pi/perl/log2dot.pl /var/www/html/files/tmp2.log > /var/www/html/files/tmp.dot');
+  }
+  $output = shell_exec('dot -Tsvg -o /var/www/html/tmp.svg /var/www/html/files/tmp.dot');
+  print "<table width='100%'>";
+  print "<tr><td width='120' valign='top' bgcolor='#b0b0b0'>";
+  #print "<select name='Protokol'><option>TCP</option></select>";
+  print "<form action='index.php'  method='GET'>";
+  print "Ausblenden<BR><input type='checkbox' name='TCP' value='1'>TCP<BR>";
+  print "<input type='checkbox' name='UDP' value='1'>UDP<BR>";
+  print "Sichtbarkeit<BR><select id='view' name='view'><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option><option value='4'>4</option><option value='5'>5</option><option value='6'>6</option><option value='7'>7</option><option value='8'>8</option><option value='9'>9</option><option value='10'>10</option></select>";
+  print "<input type='hidden' name='section' value='netoverview'>";
+  print "<BR><BR><button name='submit' type='submit' value='10_Minuten'>10_Minuten_Anzeige</button>";
+  print "<button name='submit' type='submit' value='Tagesanzeige'>Tagesanzeige</button>";
+  print "</form>";
+  print "</td>";
+  print "<td height='800'><object data='tmp.svg' type='image/svg+xml' height='100%'></td>";
+  #print "<td height='600'><img src='tmp.svg' width='100%'></td>";
+  print "</tr></table>";
+}
+if ($_GET['section']=="logfiles") {
+  $output = shell_exec('tail -50 /var/log/netvipi-eth0.log');
+  $output2 = preg_replace("/\n/", "<BR>", $output);
+  print "<table width='100%'><tr><td align='center'>$output2</td></tr></table>";
+}
+if ($_GET['section']=="dashboard") {
+  $output = shell_exec('uptime');
+  print "uptime: $output<BR>";
+  $output = shell_exec('netstat -i');
+  print "<textarea rows='8' cols='80'>netstat -i: $output</textarea><BR>";
+  print "<img src='/rrdtool/br0-day.png'><BR><img src='/rrdtool/eth0-day.png'><BR><img src='/rrdtool/eth1-day.png'><BR>";
+  #print "<table width='100%'><tr><td align='center'><img src='vnstat_h.jpg'><BR><img src='vnstat_d.jpg'></td></tr></table>";
+}
+if ($_GET['section']=="firewall") {
+  $output = shell_exec('sudo /usr/sbin/iptables -L');
+  $output2 = preg_replace("/\n/", "<BR>", $output);
+  print "<table width='100%'><tr><td align='center'>$output2</td></tr></table>";
+}
+?>
+<?php
+if (isset($_GET['section'])) {
+} else {
+  print "<table width='100%'><tr><td align='center'><img src='netvisualpi-overview.png' width='1000' height='464' alt='overview'></td></tr></table>";
+}
+?>
+</td></tr>
+<table width="100%">
+<tr bgcolor="#1F2737">
+<td>
+<table width="100%"><tr><td><a href="/dokumentation.php" target="_blank"><span style="color:white">Dokumentation</span></a></td>
+<?php
+  $output = shell_exec('ifconfig eth0 | grep "ether" | cut -d " " -f 10 | sed -e "s/://g"');
+  print "<td><a href='mailto:support@netvisualpi.de?subject=Support%20für%20netvipi$output'><span style='color:white'>Support</span></a></td><td width='250'><span style='color:white'>Serial#: netvipi$output</span></td>";
+?>
+<td width="100" align="right"><span style="color:white">Version 0.7</span></td></tr></table>
+</td>
+</tr>
+</table>
+</body>
+
+
+</html>
+
+
+
