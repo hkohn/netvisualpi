7a106a23f908be920c62ff48e76b8240d7a0e3f9
Holger Kohn <holger@kohn-nf.de>
2021-01-04 21:56:50 +0100

    /etc/rsyslog.d/ in backup und install integriert.

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 rsync -Pav /home/pi/perl ./
 rsync -Pav /home/pi/*.sh ./
 rsync -Pav --exclude=files --exclude=rrdtool /var/www/html ./
+rsync -Pav /etc/rsyslog.d/netvisualpi.conf ./
 cp /etc/iptables/rules.v* ./
 crontab -l > crontab.txt
--- install.sh
+++ install.sh
@@ -14,4 +14,5 @@ rsync -Pav /perl /home/pi/
 rsync -Pav ./*.sh /home/pi/
 rsync -Pav --exclude=files --exclude=rrdtool /html /var/www/
 rsync -Pav rules.v* /etc/iptables/
+rsync -Pav netvisualpi.conf /etc/rsyslog.d/
 # crontab -l > crontab.txt
