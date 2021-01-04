a7c96b45be9d0344b420b3705fb2af78f97e99a9
Holger Kohn <holger@kohn-nf.de>
2021-01-04 21:07:17 +0100

    crontab Zeile in install.sh auskomentiert.

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ rsync -Pav /perl /home/pi/
 rsync -Pav ./*.sh /home/pi/
 rsync -Pav --exclude=files --exclude=rrdtool /html /var/www/
 rsync -Pav rules.v* /etc/iptables/
-crontab -l > crontab.txt
+# crontab -l > crontab.txt
